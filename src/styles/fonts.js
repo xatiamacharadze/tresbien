54b872c37d26e677c93f2fdf44418741d8ab1a3b
Khatia Macharadze <kmach12@freeuni.edu.ge>
2020-10-20 00:53:19 +0400

    Fixing error with fonts

--- src/styles/fonts.js
+++ src/styles/fonts.js
@@ -1,20 +1,20 @@
-import MonumentExtendedBoldWOFF from "src/styles/fonts1/MonumentExtended-Bold.woff";
-import MonumentExtendedBoldWOFF2 from "src/styles/fonts1/MonumentExtended-Bold.woff2";
-import MonumentExtendedBoldOTF from "src/styles/fonts1/MonumentExtended-Bold.otf";
-import MonumentExtendedBoldTTF from "src/styles/fonts1/MonumentExtended-Bold.ttf";
-import MonumentExtendedBoldEOT from "src/styles/fonts1/MonumentExtended-Bold.eot";
+import MonumentExtendedBoldWOFF from "src/styles/fonts/MonumentExtended-Bold.woff";
+import MonumentExtendedBoldWOFF2 from "src/styles/fonts/MonumentExtended-Bold.woff2";
+import MonumentExtendedBoldOTF from "src/styles/fonts/MonumentExtended-Bold.otf";
+import MonumentExtendedBoldTTF from "src/styles/fonts/MonumentExtended-Bold.ttf";
+import MonumentExtendedBoldEOT from "src/styles/fonts/MonumentExtended-Bold.eot";
 
-import MonumentExtendedBookEOT from "src/styles/fonts1/MonumentExtended-Book.eot";
-import MonumentExtendedBookOTF from "src/styles/fonts1/MonumentExtended-Book.otf";
-import MonumentExtendedBookTTF from "src/styles/fonts1/MonumentExtended-Book.ttf";
-import MonumentExtendedBookWOFF from "src/styles/fonts1/MonumentExtended-Book.woff";
-import MonumentExtendedBookWOFF2 from "src/styles/fonts1/MonumentExtended-Book.woff2";
+import MonumentExtendedBookEOT from "src/styles/fonts/MonumentExtended-Book.eot";
+import MonumentExtendedBookOTF from "src/styles/fonts/MonumentExtended-Book.otf";
+import MonumentExtendedBookTTF from "src/styles/fonts/MonumentExtended-Book.ttf";
+import MonumentExtendedBookWOFF from "src/styles/fonts/MonumentExtended-Book.woff";
+import MonumentExtendedBookWOFF2 from "src/styles/fonts/MonumentExtended-Book.woff2";
 
-import MonumentExtendedMediumEOT from "src/styles/fonts1/MonumentExtended-Medium.eot";
-import MonumentExtendedMediumOTF from "src/styles/fonts1/MonumentExtended-Medium.otf";
-import MonumentExtendedMediumTTF from "src/styles/fonts1/MonumentExtended-Medium.ttf";
-import MonumentExtendedMediumWOFF from "src/styles/fonts1/MonumentExtended-Medium.woff";
-import MonumentExtendedMediumWOFF2 from "src/styles/fonts1/MonumentExtended-Medium.woff2";
+import MonumentExtendedMediumEOT from "src/styles/fonts/MonumentExtended-Medium.eot";
+import MonumentExtendedMediumOTF from "src/styles/fonts/MonumentExtended-Medium.otf";
+import MonumentExtendedMediumTTF from "src/styles/fonts/MonumentExtended-Medium.ttf";
+import MonumentExtendedMediumWOFF from "src/styles/fonts/MonumentExtended-Medium.woff";
+import MonumentExtendedMediumWOFF2 from "src/styles/fonts/MonumentExtended-Medium.woff2";
 
 export {
   MonumentExtendedBoldWOFF,
