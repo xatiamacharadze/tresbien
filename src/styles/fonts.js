c4fdfe5c158187b7239e82bbe8fb6922b166f630
Khatia Macharadze <kmach12@freeuni.edu.ge>
2020-10-20 00:33:23 +0400

    Update fonts

---
+++ src/styles/fonts.js
@@ -0,0 +1,37 @@
+import MonumentExtendedBoldWOFF from "fonts/MonumentExtended-Bold.woff";
+import MonumentExtendedBoldWOFF2 from "fonts/MonumentExtended-Bold.woff2";
+import MonumentExtendedBoldOTF from "fonts/MonumentExtended-Bold.otf";
+import MonumentExtendedBoldTTF from "fonts/MonumentExtended-Bold.ttf";
+import MonumentExtendedBoldEOT from "fonts/MonumentExtended-Bold.eot";
+
+import MonumentExtendedBookEOT from "fonts/MonumentExtended-Book.eot";
+import MonumentExtendedBookOTF from "fonts/MonumentExtended-Book.otf";
+import MonumentExtendedBookTTF from "fonts/MonumentExtended-Book.ttf";
+import MonumentExtendedBookWOFF from "fonts/MonumentExtended-Book.woff";
+import MonumentExtendedBookWOFF2 from "fonts/MonumentExtended-Book.woff2";
+
+import MonumentExtendedMediumEOT from "fonts/MonumentExtended-Medium.eot";
+import MonumentExtendedMediumOTF from "fonts/MonumentExtended-Medium.otf";
+import MonumentExtendedMediumTTF from "fonts/MonumentExtended-Medium.ttf";
+import MonumentExtendedMediumWOFF from "fonts/MonumentExtended-Medium.woff";
+import MonumentExtendedMediumWOFF2 from "fonts/MonumentExtended-Medium.woff2";
+
+export {
+  MonumentExtendedBoldWOFF,
+  MonumentExtendedBoldWOFF2,
+  MonumentExtendedBoldOTF,
+  MonumentExtendedBoldTTF,
+  MonumentExtendedBoldEOT,
+
+  MonumentExtendedBookEOT,
+  MonumentExtendedBookOTF,
+  MonumentExtendedBookTTF,
+  MonumentExtendedBookWOFF,
+  MonumentExtendedBookWOFF2,
+
+  MonumentExtendedMediumEOT,
+  MonumentExtendedMediumOTF,
+  MonumentExtendedMediumTTF,
+  MonumentExtendedMediumWOFF,
+  MonumentExtendedMediumWOFF2,
+};
